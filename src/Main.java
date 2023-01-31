caa91ca6a87ae58871c06d97ae44e082f1c13b9d
TOST2Q7 <tosteplay@gmail.com>
2023-02-01 06:26:58 +0700

    [ Test 3 ]
    - add "lwjgl v3" lib

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-
-        int var = 120;
-
-        if (var > 10) {
-            System.out.println("Тебе больше 10!");
-        }
-
-
-
+        new Game().run();
     }
 }
